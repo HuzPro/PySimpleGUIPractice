a2d46d0dbf9de5a29e3169249968b57f7018cbf0
Huz <huzaifa.tatari@Gmail.com>
2022-06-03 01:58:44 +0500

    Fully functioning calculator

--- 10 Projects/2) Calculator.py
+++ 10 Projects/2) Calculator.py
@@ -1,36 +1,49 @@
+from asyncore import read
+from tkinter import font
 import PySimpleGUI as sg
 
-menu_theme = ["Dark2::_DARK2_","DarkGreen3::_DARKGREEN3_","DarkBlue6::_DARKBLUE6_","HotDogStand::_HOTDOGSTAND_","DarkTeal10::_DARKTEAL10_","Random::_RANDOM_"]
-menu_def = [["View",["Themes", menu_theme]]]
+menu_def =  ["View", ["Theme     ", ["Dark2","DarkGreen3","DarkBlue6","HotDogStand","DarkTeal10","Random"]]]
+
 
 def window_make(theme):
     sg.theme(theme)
     sg.set_options(font="Franklin 16", button_element_size=(4,3))
     buttonSize = (6,3)
     layout = [
-        [sg.Menu(menu_def, size = (10,None),key = "-MENU-", pad = ((5,0),0))],
-        [sg.Text("Output", font="Franklin 28", justification= 'right', expand_x= True, pad = (5,(28,16),5))],
-        [sg.Button("Clear", expand_x=True, size = (4,2)), sg.Button("Enter", expand_x=True, size = (4,2))],
+        [sg.Menu([menu_def],font = "Franklin 9", size = (10,None),key = "-MENU BAR-", pad = ((5,0),0), tearoff=False)],
+        [sg.InputText("0", readonly=True, enable_events=True, disabled_readonly_text_color='#1B2631', key="-OUTPUT-", s=(10,1), expand_x=True, font="Franklin 28", justification= 'middle', pad = (5,(10,16),5))],
+        [sg.Button("Clear",k="-CLEAR-", expand_x=True, size = (4,2)), sg.Button("Enter",k="-ENTER-", bind_return_key=True, expand_x=True, size = (4,2))],
         [sg.Button("7", size = buttonSize),sg.Button("8", size = buttonSize),sg.Button("9", size = buttonSize),sg.Button("/", size = buttonSize)],
         [sg.Button("4", size = buttonSize),sg.Button("5", size = buttonSize),sg.Button("6", size = buttonSize),sg.Button("*", size = buttonSize)],
         [sg.Button("1", size = buttonSize),sg.Button("2", size = buttonSize),sg.Button("3", size = buttonSize),sg.Button("-", size = buttonSize)],
         [sg.Button("0", expand_x=True),sg.Button(".", size = buttonSize),sg.Button("+", size = buttonSize)]
-
     ]
     return sg.Window("Calculator", layout)
 
 
 window = window_make('DarkBlue9')
+mathExpression = ""
 
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    if event in ['0','1','2','3','4','5','6','7','8','9','0']:
+    if event in menu_def[1][1]:
+        window.close()
+        window = window_make(event)
+    if event in ['.','1','2','3','4','5','6','7','8','9','0']:
+        mathExpression += str(event)
+        window["-OUTPUT-"].update(mathExpression)
+    if event in ['+','-','*','/']:
+        mathExpression += str(event)
+        window["-OUTPUT-"].update(mathExpression)
+    if event == "-ENTER-":
+        window["-OUTPUT-"].update(eval(mathExpression))
+        mathExpression = str(eval(mathExpression))
+    if event == "-CLEAR-":
+        mathExpression = ""
+        window["-OUTPUT-"].update(mathExpression)
+    if event == "-OUTPUT-" + "_Enter":
         print(event)
-    if event == "-MENU-":
-        print(values["-MENU-"])
-    
-
 
 window.close()
