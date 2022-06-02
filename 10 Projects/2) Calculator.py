9a819243e0cb6264eea04cac8b471edcdb251481
Huz <35820756+HuzPro@users.noreply.github.com>
2022-06-02 14:13:25 +0500

    Update 2) Calculator.py

--- 10 Projects/2) Calculator.py
+++ 10 Projects/2) Calculator.py
@@ -5,7 +5,7 @@ def window_make(theme):
     sg.set_options(font="Franklin 16", button_element_size=(4,3))
     buttonSize = (6,3)
     layout = [
-        [sg.Button("Theme", mouseover_colors=())],
+        [sg.Button("Themes", font = ("Franklin 10"), size = (0,0),right_click_menu=menu_theme, pad = ((5,0),0))],
         [sg.Text("Output", font="Franklin 28", justification= 'right', expand_x= True, pad = (5,(28,16),5))],
         [sg.Button("Clear", expand_x=True, size = (4,2)), sg.Button("Enter", expand_x=True, size = (4,2))],
         [sg.Button("7", size = buttonSize),sg.Button("8", size = buttonSize),sg.Button("9", size = buttonSize),sg.Button("/", size = buttonSize)],
@@ -16,15 +16,18 @@ def window_make(theme):
     ]
     return sg.Window("Calculator", layout)
 
-menu_theme = ["Menu", "Dark2","DarkGreen3","DarkBlue6","HotDogStand","DarkTeal10","Random"]
+menu_theme = ["Dark2","DarkGreen3","DarkBlue6","HotDogStand","DarkTeal10","Random"]
 window = window_make('DarkBlue9')
 
 while True: 
     event, values = window.read()
-    if event == sg.WIN_CLOSED():
-        break
     if event in ['0','1','2','3','4','5','6','7','8','9','0']:
         print(event)
         sg.theme(new_theme='random')
+    elif event == "-THEME LIST-":
+        window
+    elif event == sg.WIN_CLOSED():
+        break
+
 
 window.close()
