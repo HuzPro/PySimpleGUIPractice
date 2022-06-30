2d8bf5a65393e0319c262ea7acf16ba33a1a8e24
Huz <35820756+HuzPro@users.noreply.github.com>
2022-06-30 12:32:48 +0500

    Finished, for now
    A lot of improvements can be made but I don't have time for them right now

--- 10 Projects/8) YouTube Video Downloader.py
+++ 10 Projects/8) YouTube Video Downloader.py
@@ -1,28 +1,19 @@
 from pathlib import Path
-from re import T
 import PySimpleGUI as sg
 from pytube import YouTube
 import os
 import ffmpeg
 import subprocess
 
-
-######################## NEED TO ADD HQ(1080p VIDEOS AND MERGE THEM WITH AUDIO USING PFFMPEG ########################
-# https://stackoverflow.com/questions/56973205/how-to-combine-the-video-and-audio-files-in-ffmpeg-python
-# https://stackoverflow.com/questions/62894380/download-full-hd-youtube-video-in-python
-
-
 def progressCheck(stream, chunk, bytesRemaining):
     progressAmount = 100 - round(bytesRemaining / stream.filesize * 100)
     window["-DOWNLOADPROGBAR-"].update(bar_color = "red")
     window["-DOWNLOADPROGBAR-"].update(progressAmount)
 
-
 def onCompleteCheck(stream, filePath):
     window["-DOWNLOADPROGBAR-"].update(0)
     window["-DOWNLOADPROGBAR-"].update(bar_color = "green")
     sg.popup("\n The video has been successfully downloaded! \n", title ="Download Successful!", auto_close=True, auto_close_duration=5)
-
 
 sg.theme("DarkRed1")
 
@@ -34,7 +25,6 @@ infoSection = [
     [sg.Text("Description:"), ],
     [sg.Multiline("", k="-DESCRIPTION-", p=((2,5),5), s=(75,25), no_scrollbar=True, disabled=True,)]
 ]
-
 downloadSection = [
     [sg.Frame("Highest Quality (.mp4)", [[sg.Button("Download", k="-HIGHEST-"), sg.Text("", k="-HIGHESTRES-"), sg.Text("", k="-HIGHESTSIZE-"), sg.Text("", k="-HIGHESTFPS-")]])],
     [sg.Frame("High Quality (.mp4)", [[sg.Button("Download", k="-HIGH-"), sg.Text("", k="-HIGHRES-"), sg.Text("", k="-HIGHSIZE-"), sg.Text("", k="-HIGHFPS-")]])],
@@ -53,8 +43,6 @@ main_layout = [
 ]
 
 firstLayout = [[sg.Input("", s=(50,1), p=((10,10),(10,10)), k="-INPUT-", ), sg.Button("Submit", bind_return_key=True, k="-SUBMIT-")]]
-
-
 window = sg.Window("Youtube Video Downloader", firstLayout)
 
 while True:
@@ -81,21 +69,51 @@ while True:
         window["-HIGHESTRES-"].update(f'Resolution: {video_object.streams.filter(adaptive=True)[0].resolution}')
         window["-HIGHESTFPS-"].update(f'Fps: {video_object.streams.filter(adaptive=True)[0].fps}')
         #High Quality
-        window["-HIGHSIZE-"].update(f'Size: {round(video_object.streams.filter(adaptive=True, resolution="1080p")[0].filesize / 1048576, 1)} MBs')
-        window["-HIGHRES-"].update(f'Resolution: {video_object.streams.filter(adaptive=True, resolution="1080p")[0].resolution}')
-        window["-HIGHFPS-"].update(f'Fps: {video_object.streams.filter(adaptive=True, resolution="1080p")[0].fps}')
+        try:
+            vidfilesize = round(video_object.streams.filter(adaptive=True, resolution="1080p")[0].filesize / 1048576, 1)
+            vidrez = video_object.streams.filter(adaptive=True, resolution="1080p")[0].resolution
+            vidfps = video_object.streams.filter(adaptive=True, resolution="1080p")[0].fps
+            window["-HIGHSIZE-"].update(f'Size: {vidfilesize} MBs')
+            window["-HIGHRES-"].update(f'Resolution: {vidrez}')
+            window["-HIGHFPS-"].update(f'Fps: {vidfps}')
+        except IndexError as ie:
+            window["-HIGHSIZE-"].update(f'Size: NA')
+            window["-HIGHRES-"].update(f'Resolution: NA')
+            window["-HIGHFPS-"].update(f'Fps: NA')
+            window["-HIGH-"].update(disabled=True)
+
         #Medium Quality
-        window["-MEDSIZE-"].update(f'Size: {round(video_object.streams.get_by_resolution("720p").filesize / 1048576, 1)} MBs')
-        window["-MEDRES-"].update(f'Resolution: {video_object.streams.get_by_resolution("720p").resolution}')
-        window["-MEDFPS-"].update(f'Fps: {video_object.streams.get_by_resolution("720p").fps}')
+        try:
+            vidfilesize = round(video_object.streams.get_by_resolution("720p").filesize / 1048576, 1)
+            vidrez = video_object.streams.get_by_resolution("720p").resolution
+            vidfps = video_object.streams.get_by_resolution("720p").fps
+            window["-MEDSIZE-"].update(f'Size: {vidfilesize} MBs')
+            window["-MEDRES-"].update(f'Resolution: {vidrez}')
+            window["-MEDFPS-"].update(f'Fps: {vidfps}')
+        except AttributeError as ae:
+            window["-MEDSIZE-"].update(f'Size: NA')
+            window["-MEDRES-"].update(f'Resolution: NA')
+            window["-MEDFPS-"].update(f'Fps: NA')
+            window["-MED-"].update(disabled=True)
+
         #Low Quality
-        window["-LOWSIZE-"].update(f'Size: {round(video_object.streams.get_by_resolution("360p").filesize / 1048576, 1)} MBs')
-        window["-LOWRES-"].update(f'Resolution: {video_object.streams.get_by_resolution("360p").resolution}')
-        window["-LOWFPS-"].update(f'Fps: {video_object.streams.get_by_resolution("360p").fps}')
+        if video_object.streams.get_by_resolution("360p").resolution:
+            vidfilesize = round(video_object.streams.get_by_resolution("360p").filesize / 1048576, 1)
+            vidrez = video_object.streams.get_by_resolution("360p").resolution
+            vidfps = video_object.streams.get_by_resolution("360p").fps
+        elif video_object.streams.get_by_resolution("240p").resolution:
+            vidfilesize = round(video_object.streams.get_by_resolution("240p").filesize / 1048576, 1)
+            vidrez = video_object.streams.get_by_resolution("240p").resolution
+            vidfps = video_object.streams.get_by_resolution("240p").fps
+        elif video_object.streams.get_by_resolution("144p").resolution:
+            vidfilesize = round(video_object.streams.get_by_resolution("144p").filesize / 1048576, 1)
+            vidrez = video_object.streams.get_by_resolution("144p").resolution
+            vidfps = video_object.streams.get_by_resolution("144p").fps
+        window["-LOWSIZE-"].update(f'Size: {vidfilesize} MBs')
+        window["-LOWRES-"].update(f'Resolution: {vidrez}')
+        window["-LOWFPS-"].update(f'Fps: {vidfps}')
         #Audio Only
         window["-AUDIOSIZE-"].update(f'Size: {round(video_object.streams.get_audio_only().filesize / 1048576, 1)} MBs')
-
-
 
     if event == "-HIGHEST-":
         sg.popup_ok("Type 'default' as file name to save the video with it's original file name")
@@ -104,10 +122,8 @@ while True:
         saveFilePath = os.path.dirname(saveFilePathAndFile)
         saveFileName = os.path.basename(saveFilePathAndFile)
         
-        if saveFileName == ".mp4":
-            pass
+        if saveFileName == ".mp4": pass
         elif saveFileName == "default.mp4":
-            
             videoFile = saveFilePath+"\\"+str(video_object.title)+"1.mp4"
             audioFile = saveFilePath+"\\"+str(video_object.title)+"1.mp3"
             outputFile = saveFilePath+"\\"+str(video_object.title)+".mp4"
@@ -123,15 +139,14 @@ while True:
                     highestQualityAudio = audio_stream
             highestQualityAudio.download(output_path=saveFilePath, filename=video_object.title+"1.mp3")
 
-            subprocess.run(f'ffmpeg -i "{videoFile}" -i "{audioFile}" -c copy "{outputFile}"')
+            subprocess.run(f'ffmpeg -i "{videoFile}" -i "{audioFile}" -c copy -y "{outputFile}"')
             os.remove(f"{videoFile}")
             os.remove(f"{audioFile}")
 
         else:
-
             videoFile = saveFilePath+"\\"+str(saveFileName)+"1.mp4"
             audioFile = saveFilePath+"\\"+str(saveFileName)+"1.mp3"
-            outputFile = saveFilePath+"\\"+str(saveFileName)+".mp4"
+            outputFile = saveFilePath+"\\"+str(saveFileName)
 
             for i, video_stream in enumerate(video_object.streams.filter(adaptive=True)):
                 if i == 0 and video_stream:
@@ -144,7 +159,7 @@ while True:
                     highestQualityAudio = audio_stream
             highestQualityAudio.download(output_path=saveFilePath, filename=saveFileName+"1.mp3")
 
-            subprocess.run(f'ffmpeg -i "{videoFile}" -i "{audioFile}" -c copy "{outputFile}"')
+            subprocess.run(f'ffmpeg -i "{videoFile}" -i "{audioFile}" -c copy -y "{outputFile}"')
             os.remove(f"{videoFile}")
             os.remove(f"{audioFile}")
 
@@ -155,15 +170,13 @@ while True:
         saveFilePath = os.path.dirname(saveFilePathAndFile)
         saveFileName = os.path.basename(saveFilePathAndFile)
         
-        if saveFileName == ".mp4":
-            pass
+        if saveFileName == ".mp4": pass
         elif saveFileName == "default.mp4":
-            
             videoFile = saveFilePath+"\\"+str(video_object.title)+"1.mp4"
             audioFile = saveFilePath+"\\"+str(video_object.title)+"1.mp3"
             outputFile = saveFilePath+"\\"+str(video_object.title)+".mp4"
 
-            for i, video_stream in enumerate(video_object.streams.filter(adaptive=True, resolution="1080p")):
+            for i, video_stream in enumerate(video_object.streams.filter(adaptive=True, resolution="1080p", video_codec="vp9")):
                 if i == 0 and video_stream:
                     highQualityVideo = video_stream
                     break
@@ -181,7 +194,7 @@ while True:
         else:
             videoFile = saveFilePath+"\\"+str(saveFileName)+"1.mp4"
             audioFile = saveFilePath+"\\"+str(saveFileName)+"1.mp3"
-            outputFile = saveFilePath+"\\"+str(saveFileName)+".mp4"
+            outputFile = saveFilePath+"\\"+str(saveFileName)
 
             for i, video_stream in enumerate(video_object.streams.filter(adaptive=True)):
                 if i == 0 and video_stream:
@@ -193,7 +206,6 @@ while True:
                 if i == 1 and audio_stream:
                     highQualityAudio = audio_stream
             highQualityAudio.download(output_path=saveFilePath, filename=saveFileName+"1.mp3")
-
 
             subprocess.run(f'ffmpeg -i "{videoFile}" -i "{audioFile}" -c copy "{outputFile}"')
             os.remove(f"{videoFile}")
@@ -209,7 +221,6 @@ while True:
         if saveFileName == ".mp4": pass
         elif saveFileName == "default.mp4": video_object.streams.get_by_resolution("720p").download(output_path=saveFilePath)
         else: video_object.streams.get_by_resolution("720p").download(output_path=saveFilePath, filename=saveFileName)
-
 
     if event == "-LOW-":
         sg.popup_ok("Type 'default' as file name to save the video with it's original file name")
@@ -230,6 +241,5 @@ while True:
         if saveFileName == ".mp3": pass
         elif saveFileName == "default.mp3": video_object.streams.get_audio_only().download(output_path=saveFilePath)
         else: video_object.streams.get_audio_only().download(output_path=saveFilePath, filename=saveFileName)
-
 
 window.close()
